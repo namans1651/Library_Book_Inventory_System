b260f1a11608dcb832ba044105d99a396e56e98a
Naman Sharma <naman.sharma1_cs19@gla.ac.in>
2020-08-30 19:49:39 +0530

    final commit of Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -4,18 +4,13 @@
  *   Time: 3:46 PM
  *   File: Library.java
  */
-
 package definitions;
-
 public class Library {
     private Book[] currentlyAvailabeBooks;
-
     public Book[] getCurrentlyAvailabeBooks() {
         return currentlyAvailabeBooks;
     }
-
     public void setCurrentlyAvailabeBooks(Book[] currentlyAvailabeBooks) {
         this.currentlyAvailabeBooks = currentlyAvailabeBooks;
     }
-
 }
