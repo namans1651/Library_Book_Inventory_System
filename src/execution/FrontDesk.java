ad1474162b749efcbc76d39c90d4c738502afa5f
Naman Sharma <naman.sharma1_cs19@gla.ac.in>
2020-08-30 16:39:28 +0530

    we have created constructors

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,18 +6,17 @@
  */
 
 package execution;
+
 import definitions.Student;
 
 public class FrontDesk {
     public static void main(String[] args) {
         Student student = new Student();
-
         System.out.println(student.getFirstName());
         System.out.println(student.getMiddleName());
         System.out.println(student.getLastName());
         System.out.println(student.getStudentRollNumber());
         System.out.println(student.getNumberOfBooksIssuedByStudent());
         System.out.println(student.getNamesOfBookIssuedByStudent());
-
     }
 }
