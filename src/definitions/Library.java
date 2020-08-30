8ad18c7a53b7e240bb8e09537984718e1f11fd79
Naman Sharma <naman.sharma1_cs19@gla.ac.in>
2020-08-30 15:29:25 +0530

    add a class Library.java

---
+++ src/definitions/Library.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Sharma
+ *   Date: 8/30/2020
+ *   Time: 3:29 PM
+ *   File: Library.java
+ */
+
+package definitions;
+
+public class Library {
+}
