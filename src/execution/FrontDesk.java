27cc3d8cadfef2c78ccd589212dff7de4ca603b6
Naman Sharma <naman.sharma1_cs19@gla.ac.in>
2020-08-30 16:13:20 +0530

    create a execution class and write a main method inside it

---
+++ src/execution/FrontDesk.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Sharma
+ *   Date: 8/30/2020
+ *   Time: 4:12 PM
+ *   File: FrontDesk.java
+ */
+
+package execution;
+
+public class FrontDesk {
+    public static void main(String[] args) {
+
+    }
+}
